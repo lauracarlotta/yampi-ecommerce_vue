140e2bcc861f063f30777bcb51075022a0fbf0e5
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-25 22:56:26 -0300

    Remove files not use and init header component

---
+++ src/components/NavbarHeader/NavbarHeader.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<nav>
+			<img src="https://i.ibb.co/Vj7LVmk/yampi-logo.png" alt="">
+			<input type="search">
+			<input id="search-button" type="button">
+
+			<input id="shopping-button"  type="button">
+		</nav>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NavbarHeader',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" src="./NavbarHeader.scss" scoped/>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,17 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<NavbarHeader/>
 	</div>
 </template>
 
 <script>
-// @ is an alias to /src
 // eslint-disable-next-line import/no-unresolved
-import HelloWorld from '@/components/HelloWorld.vue';
+import NavbarHeader from '@/components/NavbarHeader/NavbarHeader.vue';
 
 export default {
 	name: 'HomeView',
 	components: {
-		HelloWorld,
+		NavbarHeader,
 	},
 };
 </script>
