f6f39e28bded2cdf929a66662152278747566056
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-26 05:14:17 -0300

    WIP: Init create style header - desktop

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,55 @@
 <template>
-	<nav>
-		<router-link to="/">Home</router-link> |
-		<router-link to="/about">About</router-link>
-	</nav>
-	<router-view />
+	<header>
+		<div class="container">
+			<div id="yampi-header">
+				<img class="yampi-logo" src="https://i.ibb.co/Vj7LVmk/yampi-logo.png" alt="">
+				<div id="yampi-header__search">
+					<input id="input-search__main" type="search" placeholder="O que você está procurando? ">
+					<i class="fa-solid fa-magnifying-glass"></i>
+				</div>
+				<div id="yampi-header__shopping">
+					<i class="fa-solid fa-cart-shopping"></i>
+					<div>3 itens</div>
+					<div>R$ 2.578,70</div>
+				</div>
+			</div>
+			<nav style="margin-top: 2rem">
+				<div id="yampi-nav">
+					<router-link class="yampi-nav__link" to="/">Electronics</router-link>
+					<router-link class="yampi-nav__link" to="/">Jewelery</router-link>
+					<router-link class="yampi-nav__link" to="/">Men's Clothing</router-link>
+					<router-link class="yampi-nav__link" to="/">Women's Clothing</router-link>
+				</div>
+			</nav>
+		</div>
+	</header>
+	<main>
+		<router-view />
+	</main>
+	<footer>
+		<div class="container">
+			<div>
+				<img src="https://i.ibb.co/Vj7LVmk/yampi-logo.png" alt="">
+			</div>
+
+			<div>
+				<ul>Institucional
+					<li>Quem somos</li>
+					<li>Política de privacidade</li>
+					<li>Política de compra</li>
+					<li>Política de troca e devolução</li>
+					<li>Nossas lojas</li>
+				</ul>
+			</div>
+
+			<div>
+				<ul>Ajuda
+					<li>Fale conosco</li>
+					<li>Perguntas frequentes</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
 </template>
 
 <style lang="scss">
@@ -15,16 +61,99 @@
 	color: #2c3e50;
 }
 
-nav {
-	padding: 30px;
+* {
+	box-sizing: border-box;
+	font-family: Helvetica, sans-serif;
+	margin:0;
+	padding: 0;
+}
 
-	a {
-		font-weight: bold;
-		color: #2c3e50;
+header {
+	background-color: #f3f4f6;
+	border-bottom: .25rem solid #dedaf1;
 
-		&.router-link-exact-active {
-			color: #42b983;
+	.container {
+		margin: 0 auto;
+		width: 1120px;
+
+		#yampi-header {
+			align-items: center;
+			display: flex;
+			justify-content: space-between;
+
+			i {
+				color: #725bc2;
+			}
+
+			.yampi-logo {
+				width: 200px;
+			}
+
+			#yampi-header__search {
+
+				#input-search__main {
+					border-radius: .5rem;
+					border: 2px solid #8e7dcf;
+					padding: .75rem 1.5rem;
+					width: 550px;
+					max-width: 700px;
+				}
+
+				i {
+					position: relative;
+					top: 4px;
+					right: 36px;
+					cursor: pointer;
+					font-size: 1.5rem;
+				}
+
+			}
+
+			#yampi-header__shopping {
+				background-clip: #efecf8;
+				padding: 1rem;
+				margin-top: 1rem;
+				border-radius: 1rem;
+				display: flex;
+				flex-direction: column;
+				box-shadow: 0 10px 15px -3px rgba(0,0,0,0.1),
+							0 4px 6px -2px rgba(0,0,0,0.05);
+				cursor: pointer;
+				transition: all 500ms ease;
+
+				i {
+					margin-bottom: 1rem;
+					font-size: 2rem;
+				}
+
+				&:hover {
+					background-color: #dedaf1;
+				}
+			}
+		}
+
+		#yampi-nav {
+			display: flex;
+			align-items: stretch;
+			flex-direction: row;
+			justify-content: space-between;
+			padding: 0 1rem;
+
+			.yampi-nav__link {
+				color: #374151;
+				text-decoration: none;
+				text-transform: uppercase;
+				font-weight: bolder;
+				padding: 1rem 0;
+				width: 25%;
+
+				&:hover {
+					border-radius: 1rem 1rem 0 0;
+					background-color: #dedaf1;
+				}
+			}
 		}
 	}
 }
+
 </style>
