e51aad520e68ba8172bec6992bdada52fd4ce563
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-26 03:29:43 -0300

    Fix error project eslint

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+
+	transpileDependencies: true,
+
+	pages: {
+		index: {
+			entry: 'src/main.js',
+			title: 'Yampi Ecommerce',
+		},
+	},
+});
