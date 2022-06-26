51eb3db52724b13bd5c8239d876eac386e00cd2f
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-25 22:37:50 -0300

    WIP: Start project - add sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -70,17 +70,24 @@ h3 {
 	margin: 40px 0 0;
 }
 
+p {
+	a {
+		color: #42b983;
+	}
+}
+
 ul {
 	list-style-type: none;
 	padding: 0;
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+
+		a {
+			color: #42b983;
+		}
+	}
 }
 
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
-
-a {
-	color: #42b983;
-}
 </style>
